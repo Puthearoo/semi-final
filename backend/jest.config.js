1421ff4556b09c96e1451443bcfc50c611697b7b
user <ASUS@MSU.YELLOW>
2025-09-18 10:11:06 +0700

    fix: remove type module and use CommonJS

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
   testEnvironment: 'node',
   transform: {
     '^.+\\.js$': 'babel-jest'
-  }
+  },
+  forceExit: true, // Add this line
+  testTimeout: 30000 // Optional: increase timeout if needed
 };
